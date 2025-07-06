0a4ccf6f82c1a83b3e000a296a0983693475d1b9
twilight <86181150@qq.com>
2025-07-06 11:15:39 +0800

    处理一行报文函数待完成

--- httpd.cpp
+++ httpd.cpp
@@ -1,3 +1,4 @@
+// 写在前面，虽然是使用cpp重写，但是并没有使用面向对象的思想，毕竟这一份主要在于理解webserver在cpp中的实现，后面会使用面向对象的思想来重写
 #include <iostream>         // 输入输出操作
 #include <string>           // 字符串处理
 #include <vector>           // 动态数组
@@ -39,6 +40,77 @@ void error_exit(string message) {
     exit(0);
 }
 
+// 获取一行HTTP报文
+int getHttpLine(int client_socket, vector<char> &buffer, int buffer_size = -1) {
+	if(buffer_size == -1) {
+		buffer_size = buffer.size();
+	}
+	
+	int i = 0;
+	char c = '\0';
+	int n = 0;
+
+	while(i < buffer_size && c != '\0') {
+		n = recv(client_socket, &c, 1, 0);
+
+		if(n > 0) {
+			if(c == '\r') {
+				n = recv(client_socket, &c, 1, MSG_PEEK);
+				if(n > 0 && c == '\n') {
+					n = recv(client_socket, &c, 1, 0);
+				}
+				else {
+					c = '\0';
+				}
+			}
+		}
+	}
+
+}
+
+void header(int client_socket) {
+	vector<char> buffer(1024);
+	sprintf(buffer.data(), "HTTP/1.1 200 OK\r\n");
+	send(client_socket, buffer.data(), strlen(buffer.data()), 0);
+
+	sprintf(buffer.data(), "Server: MyPoorWebServer\r\n");
+	send(client_socket, buffer.data(), strlen(buffer.data()), 0);
+
+	sprintf(buffer.data(), "Content-Type: text/html\r\n");
+	send(client_socket, buffer.data(), strlen(buffer.data()), 0);
+
+	sprintf(buffer.data(), "\r\n");
+	send(client_socket, buffer.data(), strlen(buffer.data()), 0);
+}
+
+void *accept_request(int client_socket) {
+	vector<char> buffer(1024);
+	int n = 1;
+	while(n > 0) {
+		n = recv(client_socket, buffer.data(), sizeof(buffer), 0); // 这个是阻塞的，读完就卡住了
+		cout << buffer.data();
+		
+	}
+
+	header(client_socket);
+
+	ifstream ifile;
+
+	ifile.open("httpdocs/test.html", ios::binary);
+	if(!ifile) {
+		error_exit("file error");
+	}
+	ifile.seekg(0, ios::beg);
+
+	while(!ifile.eof()) {
+		ifile.read(buffer.data(), sizeof(buffer));
+		send(client_socket, buffer.data(), ifile.gcount(), 0);
+	}
+
+    close(client_socket);
+    return NULL;
+}
+
 int startSever(unsigned short &port) {
 	if(port == 0) {
 		port = 8080;
@@ -52,14 +124,16 @@ int startSever(unsigned short &port) {
 	name.sin_port = htons(port);
 	name.sin_addr.s_addr = htonl(INADDR_ANY);
 
+	// 设置端口复用，这个要放在绑定前面
+	int opt = 1;
+	if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+		error_exit("setsockopt error");
+	}
+
 	if(bind(server_socket, (sockaddr*)&name, name_len) < 0) {
 		error_exit("bind error");
 	}
-	// 设置端口复用
-	int opt = 1;
-	if(setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(&opt)) < 0) {
-		error_exit("setsockopt error");
-	}
+
 	// 开始监听
 	if(listen(server_socket, 10) < 0) {
 		error_exit("listen error");
@@ -78,7 +152,7 @@ int main() {
 	int client_socket = -1;
 	sockaddr_in client_name;
 	socklen_t client_name_len = sizeof(client_name);
-	thread new_thread;
+	// thread new_thread;
 	server_socket = startSever(port);
 
 	while(1) {
@@ -88,7 +162,8 @@ int main() {
 		if(client_socket < 0) {
 			error_exit("accept error");
 		}
-		
+		thread new_thread(accept_request, client_socket);
+		new_thread.detach();
 	}
 
     return 0;
